4957cf648052783a3a961df86b4c99c951592f83
macbook_air <2477952281@qq.com>
2025-06-26 22:54:50 +0800

    8638直接插入排序

---
+++ SCAU_OJ/实验6/8638直接插入排序/8638直接插入排序.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+int main() {
+  int n;
+  std::cin >> n;
+  std::vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    std::cin >> arr[i];
+  }
+  for (int i = 1; i < n; i++) {
+    int temp = i;
+    while (temp > 0 && arr[temp] < arr[temp - 1]) {
+      std::swap(arr[temp], arr[temp - 1]);
+      temp--;
+    }
+    // 排序一趟打印一次
+    for (int i : arr) {
+      std::cout << i << " ";
+    }
+    std::cout << std::endl;
+  }
+}
